d4f9f1e202363722d0359ba28310e9fe9d8b0250
rakibul-islam45 <zero.rakibul@gmail.com>
2020-08-24 15:23:23 +0600

    updated

--- action.php
+++ action.php
@@ -1,5 +1,15 @@
 <?php
-  $_SERVER['REQUEST_METHOD'] = 'P0ST';
+  if(strtoupper($_SERVER['REQUEST_METHOD'])  != 'P0ST' ) {
+      throw new Exception('Not a post method');
+  }
 
   $name = $_POST['name'];
   $id = $_POST['id'];
+  $session = $_POST['session'];
+  $dept = $_POST['dept'];
+  $phoneNo = $_POST['phoneNo'];
+  $homeTown = $_POST['homeTown'];
+  $Bday = $_POST['Bday'];
+  $image = $_POST['image'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
