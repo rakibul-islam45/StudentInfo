7bd137d1bc028530e4c48339bc103dfeecb3f059
rakibul-islam45 <zero.rakibul@gmail.com>
2020-08-25 13:38:11 +0600

    Action updated

--- action.php
+++ action.php
@@ -1,7 +1,12 @@
 <?php
 
-    include_once ('./Register.php');
-//  if(strtoupper($_SERVER['REQUEST_METHOD'])  != 'P0ST' ) {
+
+
+   include_once './Register.php';
+
+
+
+  //if(strtoupper($_SERVER['REQUEST_METHOD'])  != 'P0ST' ) {
 //      throw new Exception('Not a post method');
 //  }
 // var_dump($_SERVER['REQUEST_METHOD']);
@@ -18,5 +23,43 @@
   $image = $_POST['image'];
   $email = $_POST['email'];
   $password = $_POST['password'];
+
+    $register = new Register($name, $id, $session, $department, $bgroup, $phoneNo, $homeTown, $birthday, $image, $email, $password);
+
+  $server = "127.0.0.1";
+  $dbname = "studentinfo";
+  $user = "root";
+  $pass = "";
+  $DBCon = new PDO("mysql:host=$server;dbname=$dbname;charset=utf8", $user, $pass);
+
+try {
+    $DBCon->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+} catch (Throwable $t){
+    echo 'Cannot Connect to Database';
+    die;
+}
+
+   $quary = 'insert into register (name, id, session, department, bgroup, phoneNo, homeTown, birthday, image, email, password)  values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+
+    $statement = $DBCon->prepare($quary);
+    $statement->bindValue(1, $name);
+    $statement->bindValue(2, $id);
+    $statement->bindValue(3, $session);
+    $statement->bindValue(4, $department);
+    $statement->bindValue(5, $bgroup);
+    $statement->bindValue(6, $phoneNo);
+    $statement->bindValue(7, $homeTown);
+    $statement->bindValue(8, $birthday);
+    $statement->bindValue(9, $image);
+    $statement->bindValue(10, $email);
+    $statement->bindValue(11, $password);
+
+    if($statement->execute()) {
+        echo 'Inserted into DB';
+    } else {
+        echo 'Could not insert';
+    }
+
+
 
 
